7022bba2291ac54a5a899d5bc255cbfcdfd6a222
Thomas Brown <github@honourity.com>
2021-11-12 01:14:46 +1100

    moved graph generating into its own method for easier readability

--- Assets/Scripts/BigBugManager.cs
+++ Assets/Scripts/BigBugManager.cs
@@ -10,7 +10,7 @@ public class BigBugManager : Singleton<BigBugManager>
     [SerializeField]
     private bool _spawnBugs;
 
-    private int _environmentLayerId = 6;
+    private readonly int _environmentLayerId = 6;
 
     [SerializeField]
     private float _bugSpawnPeriod = 10f;
@@ -27,104 +27,7 @@ public class BigBugManager : Singleton<BigBugManager>
 
         if (_spawnBugs && Nodes.Count == 0)
         {
-            //assembling point nodes based on mesh vertices (and storing normals for rotation of pathing entities)
-            var environmentObjects = FindGameObjectsInLayer(_environmentLayerId);
-            foreach (var environmentObject in environmentObjects)
-            {
-                Matrix4x4 localToWorld = environmentObject.transform.localToWorldMatrix;
-
-                var meshfilter = environmentObject.GetComponent<MeshFilter>();
-                if (meshfilter != null)
-                {
-                    for (int i = 0; i < meshfilter.mesh.vertices.Length; ++i)
-                    {
-                        var vertex = localToWorld.MultiplyPoint3x4(meshfilter.mesh.vertices[i]);
-                        var normal = localToWorld.MultiplyPoint3x4(meshfilter.mesh.normals[i]);
-
-                        Nodes.Add(new Node
-                        {
-                            mesh = meshfilter.mesh,
-                            vertexId = i,
-                            Point = vertex,
-                            Normal = normal
-                        });
-                    }
-                }
-            }
-
-            //linking nodes to each other based on mesh triangles
-            foreach (var node in Nodes)
-            {
-                var triangles = node.mesh.triangles;
-                if (triangles.Length > 1)
-                {
-                    for (int i = 0; i < triangles.Length; i++)
-                    {
-                        if (triangles[i] == node.vertexId)
-                        {
-                            var position = i % 3;
-                            if (position == 0)
-                            {
-                                var destinationNodeA = Nodes.FirstOrDefault(n => n.vertexId == triangles[i + 1]);
-                                node.Children.Add(destinationNodeA);
-                                destinationNodeA.Children.Add(node);
-
-                                var destinationNodeB = Nodes.FirstOrDefault(n => n.vertexId == triangles[i + 2]);
-                                node.Children.Add(destinationNodeB);
-                                destinationNodeB.Children.Add(node);
-                            }
-                            else if (position == 1)
-                            {
-                                var destinationNodeA = Nodes.FirstOrDefault(n => n.vertexId == triangles[i - 1]);
-                                node.Children.Add(destinationNodeA);
-                                destinationNodeA.Children.Add(node);
-
-                                var destinationNodeB = Nodes.FirstOrDefault(n => n.vertexId == triangles[i + 1]);
-                                node.Children.Add(destinationNodeB);
-                                destinationNodeB.Children.Add(node);
-                            }
-                            else if (position == 2)
-                            {
-                                var destinationNodeA = Nodes.FirstOrDefault(n => n.vertexId == triangles[i - 1]);
-                                node.Children.Add(destinationNodeA);
-                                destinationNodeA.Children.Add(node);
-
-                                var destinationNodeB = Nodes.FirstOrDefault(n => n.vertexId == triangles[i - 2]);
-                                node.Children.Add(destinationNodeB);
-                                destinationNodeB.Children.Add(node);
-                            }
-                        }
-                    }
-                }
-            }
-
-            //merge close nodes together (will orphan the other close nodes, which will then be dumped)
-            foreach (var node in Nodes)
-            {
-                foreach (var node2 in Nodes)
-                {
-                    if (node != node2 && Vector3.Distance(node.Point, node2.Point) < 0.1f)
-                    {
-                        //merge
-                        if (node2.Children.Count() > 0)
-                        {
-                            node.Children.AddRange(node2.Children);
-
-                            //swap children's links
-                            foreach (var child in node2.Children)
-                            {
-                                child.Children = child.Children.Where(c => c != node2).ToList();
-                                child.Children.Add(node);
-                            }
-
-                            node2.Children.Clear();
-                        }
-                    }
-                }
-            }
-
-            //dump any vertices which dont have any edges to link to
-            Nodes = Nodes.Where(n => n.Children.Count > 0).ToList();
+            PopulateGraph();
         }
     }
 
@@ -159,6 +62,111 @@ public class BigBugManager : Singleton<BigBugManager>
             return null;
         }
         return environmentGameObjects;
+    }
+
+    private void PopulateGraph()
+    {
+        //wipe any existing graph data
+        Nodes.Clear();
+
+        //assembling point nodes based on mesh vertices (and storing normals for rotation of pathing entities)
+        var environmentObjects = FindGameObjectsInLayer(_environmentLayerId);
+        foreach (var environmentObject in environmentObjects)
+        {
+            Matrix4x4 localToWorld = environmentObject.transform.localToWorldMatrix;
+
+            var meshfilter = environmentObject.GetComponent<MeshFilter>();
+            if (meshfilter != null)
+            {
+                for (int i = 0; i < meshfilter.mesh.vertices.Length; ++i)
+                {
+                    var vertex = localToWorld.MultiplyPoint3x4(meshfilter.mesh.vertices[i]);
+                    var normal = localToWorld.MultiplyPoint3x4(meshfilter.mesh.normals[i]);
+
+                    Nodes.Add(new Node
+                    {
+                        mesh = meshfilter.mesh,
+                        vertexId = i,
+                        Point = vertex,
+                        Normal = normal
+                    });
+                }
+            }
+        }
+
+        //linking nodes to each other based on mesh triangles
+        foreach (var node in Nodes)
+        {
+            var triangles = node.mesh.triangles;
+            if (triangles.Length > 1)
+            {
+                for (int i = 0; i < triangles.Length; i++)
+                {
+                    if (triangles[i] == node.vertexId)
+                    {
+                        var position = i % 3;
+                        if (position == 0)
+                        {
+                            var destinationNodeA = Nodes.FirstOrDefault(n => n.vertexId == triangles[i + 1]);
+                            node.Children.Add(destinationNodeA);
+                            destinationNodeA.Children.Add(node);
+
+                            var destinationNodeB = Nodes.FirstOrDefault(n => n.vertexId == triangles[i + 2]);
+                            node.Children.Add(destinationNodeB);
+                            destinationNodeB.Children.Add(node);
+                        }
+                        else if (position == 1)
+                        {
+                            var destinationNodeA = Nodes.FirstOrDefault(n => n.vertexId == triangles[i - 1]);
+                            node.Children.Add(destinationNodeA);
+                            destinationNodeA.Children.Add(node);
+
+                            var destinationNodeB = Nodes.FirstOrDefault(n => n.vertexId == triangles[i + 1]);
+                            node.Children.Add(destinationNodeB);
+                            destinationNodeB.Children.Add(node);
+                        }
+                        else if (position == 2)
+                        {
+                            var destinationNodeA = Nodes.FirstOrDefault(n => n.vertexId == triangles[i - 1]);
+                            node.Children.Add(destinationNodeA);
+                            destinationNodeA.Children.Add(node);
+
+                            var destinationNodeB = Nodes.FirstOrDefault(n => n.vertexId == triangles[i - 2]);
+                            node.Children.Add(destinationNodeB);
+                            destinationNodeB.Children.Add(node);
+                        }
+                    }
+                }
+            }
+        }
+
+        //merge close nodes together (will orphan the other close nodes, which will then be dumped)
+        foreach (var node in Nodes)
+        {
+            foreach (var node2 in Nodes)
+            {
+                if (node != node2 && Vector3.Distance(node.Point, node2.Point) < 0.1f)
+                {
+                    //merge
+                    if (node2.Children.Count() > 0)
+                    {
+                        node.Children.AddRange(node2.Children);
+
+                        //swap children's links
+                        foreach (var child in node2.Children)
+                        {
+                            child.Children = child.Children.Where(c => c != node2).ToList();
+                            child.Children.Add(node);
+                        }
+
+                        node2.Children.Clear();
+                    }
+                }
+            }
+        }
+
+        //dump any vertices which dont have any edges to link to
+        Nodes = Nodes.Where(n => n.Children.Count > 0).ToList();
     }
 
     public class Node
